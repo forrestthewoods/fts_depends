e7d7c5c1eeee12d29f510da460838bcd4bbdad29
Forrest Smith <forrestthewoods@gmail.com>
2022-10-15 11:36:55 -0700

    progress

--- src/main.rs
+++ src/main.rs
@@ -26,15 +26,66 @@ struct Dependency {
     children: Vec<Box<Dependency>>,
 }
 
+#[derive(thiserror::Error, Debug)]
+enum DependsError {
+    #[error("Skipped system file")]
+    SkippedSystem,
+
+    #[error("⚠️ File not found ⚠️")]
+    NotFound,
+}
+
+fn printer(table: &mut prettytable::Table, dep: &Dependency, depth: i32) {
+    let loc_str = match &dep.path {
+        Some(path) => path.to_string_lossy().into_owned(),
+        None => "⚠️ Not Found ⚠️".to_owned(),
+    };
+    table.add_row(prettytable::row![
+        &dep.name.to_string_lossy(),
+        loc_str.as_str()
+    ]);
+
+    for child in &dep.children {
+        printer(table, child, depth + 1);
+    }
+}
+
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
     // Get dumpbin path
-    let dumpbin = match args.dumpbin {
-        Some(path) => Ok(path),
+    let dumpbin: PathBuf = match &args.dumpbin {
+        Some(path) => Ok(path.clone()),
         None => find_dumpbin(),
     }?;
 
+    let target_dir: PathBuf = args
+        .target
+        .parent()
+        .expect(&format!(
+            "Failed to get parent dir for [{:?}]",
+            &args.target
+        ))
+        .into();
+
+    let mut visited: HashSet<PathBuf> = Default::default();
+    visited.insert(args.target.file_name().unwrap().into());
+
+    let deps = find_deps(&dumpbin, &args.target, &target_dir, &args, &mut visited);
+
+    let mut table = prettytable::Table::new();
+    table.set_titles(prettytable::row![
+        "Dependency",
+        "Resolved Location (best guess)"
+    ]);
+    //table.add_row(prettytable::row!["Dep", "Loc"]);
+    for dep in &deps {
+        printer(&mut table, dep, 0);
+    }
+    table.set_format(*prettytable::format::consts::FORMAT_NO_LINESEP_WITH_TITLE);
+    table.printstd();
+
+    /*
     // Build command
     let mut cmd = std::process::Command::new(&dumpbin);
     cmd.arg("/DEPENDENTS");
@@ -77,21 +128,39 @@ fn main() -> anyhow::Result<()> {
     }
     table.set_format(*prettytable::format::consts::FORMAT_NO_LINESEP_WITH_TITLE);
     table.printstd();
+    */
 
     Ok(())
 }
+
+// algorithm
+// find dumpbin
+// run dumpbin on initial target
+// get list of dependency filenames
+// cull system and dupes
+// repeat for child
 
 fn find_deps(
     dumpbin: &Path,
     target: &Path,
     target_dir: &Path,
     args: &Args,
-    found_deps: &mut HashSet<PathBuf>,
-) -> anyhow::Result<Dependency> {
+    visited: &mut HashSet<PathBuf>,
+) -> anyhow::Result<Box<Dependency>> {
+    let target_loc = find_location(target, target_dir).or(Err(DependsError::NotFound))?;
+
+    if !args.show_system {
+        let lossy_loc = target_loc.to_string_lossy().to_lowercase();
+        if lossy_loc.contains("windows\\system32") {
+            println!("Skipping system lib: {lossy_loc}");
+            Err(DependsError::SkippedSystem)?;
+        }
+    }
+
     // Build command
     let mut cmd = std::process::Command::new(&dumpbin);
     cmd.arg("/DEPENDENTS");
-    cmd.arg(args.target.as_os_str());
+    cmd.arg(target_loc.as_os_str());
 
     // Invoke command and capture output
     let output = cmd
@@ -100,30 +169,59 @@ fn find_deps(
     let stdout = String::from_utf8(output.stdout)?;
 
     // Extract dependencies from stdout
-    let deps = extract_deps(&stdout);
+    let deps = extract_deps(&stdout)?;
 
     let mut result: Box<Dependency> = Default::default();
-    result.name = target.file_name().unwrap().into();
-    //result.
+    result.name = target_loc.file_name().unwrap().into();
+    result.path = Some(target_loc);
 
-    anyhow::bail!("Oh no")
+    let target_loc_dir: PathBuf = target
+        .parent()
+        .expect(&format!(
+            "Failed to get parent dir for [{:?}]",
+            &args.target
+        ))
+        .into();
+
+    for dep in deps {
+        let inserted = visited.insert(dep.into());
+        if !inserted {
+            continue;
+        }
+
+        match find_deps(dumpbin, &PathBuf::from(dep), &target_loc_dir, args, visited) {
+            Ok(recursive_dep) => result.children.push(recursive_dep),
+            Err(err) => {
+                match err.downcast_ref::<DependsError>() {
+                    Some(DependsError::SkippedSystem) => continue,
+                    Some(DependsError::NotFound) | None => (),
+                }
+                result.children.push(Box::new(Dependency {
+                    name: dep.into(),
+                    path: None,
+                    children: Default::default(),
+                }))
+            }
+        };
+    }
+
+    Ok(result)
 }
 
 fn find_location(target: &Path, target_dir: &Path) -> which::Result<PathBuf> {
-    which::which_in(target, Some(&target_dir), &target_dir).or_else(|_| which::which(target))
+    let cwd = "";
+    which::which_in(target, Some(target_dir), cwd).or_else(|_| which::which(target))
 }
 
-fn extract_deps(dumpbin_str: &str) -> Vec<&str> {
+fn extract_deps(dumpbin_str: &str) -> anyhow::Result<Vec<&str>> {
     // Extract dependencies
     let start_str = "Image has the following dependencies:";
     let end_str = "Summary";
 
     let start_idx = dumpbin_str
         .find(start_str)
-        .expect("Failed to find start_str in stdout");
-    let end_idx = dumpbin_str
-        .find(end_str)
-        .expect("Failed to find end_str in stdout");
+        .ok_or_else(|| anyhow::anyhow!("no dependencies"))?;
+    let end_idx = dumpbin_str.find(end_str).expect("failed to find summary");
 
     let deps_str = dumpbin_str[start_idx + start_str.len()..end_idx].trim();
     let deps: Vec<&str> = deps_str
@@ -133,7 +231,7 @@ fn extract_deps(dumpbin_str: &str) -> Vec<&str> {
         .filter(|line| *line != "Image has the following delay load dependencies:")
         .collect();
 
-    deps
+    Ok(deps)
 }
 
 fn find_dumpbin() -> anyhow::Result<PathBuf> {
