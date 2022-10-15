54c691435d03de9e87b34f436b7cfadddc6204ab
Forrest Smith <forrestthewoods@gmail.com>
2022-10-14 22:41:23 -0700

    good progress!

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use std::io::Write;
 use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
@@ -18,71 +19,19 @@ fn main() -> anyhow::Result<()> {
         Some(path) => Ok(path),
         None => find_dumpbin(),
     }?;
-    println!("dumpbin: {:?}", dumpbin);
-
-
-    // Get dumpbin.exe dir
-    let dumpbin_dir = dumpbin.parent().expect("Failed to get parent dir");
-    println!("dumpbin_dir: {:?}", dumpbin_dir);
-
-    let filename = dumpbin.file_name().expect("Failed to get filename");
-    println!("dumpbin_filename: {:?}", filename);
-
-    let target_str = args.target.to_string_lossy();
-
-    let cmd = std::process::Command::new(&filename)
-        .args(["/DEPENDENTS", &target_str])
-        .current_dir(dumpbin_dir);
-
-
-    let output = cmd.output()
-        .expect("failed invoke dumpbin.exe");
-
-    println!("{:?}", output);
 
     // Build command
-    /*
-    let mut cmd = OsString::new();
-    //cmd.push("\"");
-    cmd.push(dumpbin.as_os_str());
-    cmd.push(" /?");
-    //cmd.push("\" /DEPENDENTS ");
-    //cmd.push(args.target.as_os_str());
-    */
-
-    //println!("Invoking command: {:?}", &cmd);
+    let mut cmd = std::process::Command::new(&dumpbin);
+    cmd.arg("/DEPENDENTS");
+    cmd.arg(args.target.as_os_str());
 
     // Invoke command and capture output
-    /*
-    let out = subprocess::Exec::cmd(cmd)
-        .stdout(subprocess::Redirection::Pipe)
-        .capture()?
-        .stdout_str();
-    */
+    let output = cmd.output().expect(&format!("failed to run [{:?}]", &dumpbin));
 
-    /*
-    std::env::set_current_dir("C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\VC\\Tools\\MSVC\\14.32.31326\\bin\\Hostx64\\x64")?;
-    let mut p = subprocess::Popen::create(
-        &["dumpbin.exe", "c:/stuff/path/cvdump.exe"],
-        subprocess::PopenConfig {
-            stdout: subprocess::Redirection::Pipe,
-            ..Default::default()
-        },
-    )?;
-
-    // Obtain the output from the standard streams.
-    let (out, err) = p.communicate(None)?;
-
-    if let Some(exit_status) = p.poll() {
-        // the process has finished
-    } else {
-        // it is still running, terminate it
-        p.terminate()?;
-    }
-
-    println!("cout: {:?}", out);
-    println!("cerr: {:?}", err);
-    */
+    // Print results
+    println!("status: {}", output.status);
+    std::io::stdout().write_all(&output.stdout).unwrap();
+    std::io::stderr().write_all(&output.stderr).unwrap();
 
     Ok(())
 }
