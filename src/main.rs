cf6350944339b59daac7e7ebd68f811ef932156e
Forrest Smith <forrestthewoods@gmail.com>
2022-10-14 23:31:35 -0700

    pretty table

--- src/main.rs
+++ src/main.rs
@@ -25,21 +25,59 @@ fn main() -> anyhow::Result<()> {
     cmd.arg(args.target.as_os_str());
 
     // Invoke command and capture output
-    let output = cmd.output().expect(&format!("failed to run [{:?}]", &dumpbin));
+    let output = cmd
+        .output()
+        .expect(&format!("failed to run [{:?}]", &dumpbin));
 
     let stdout = String::from_utf8(output.stdout)?;
 
     // Extract dependencies
     let start_str = "Image has the following dependencies:";
     let end_str = "Summary";
-    
-    let start_idx = stdout.find(start_str).expect("Failed to find start_str in stdout");
-    let end_idx = stdout.find(end_str).expect("Failed to find end_str in stdout");
+
+    let start_idx = stdout
+        .find(start_str)
+        .expect("Failed to find start_str in stdout");
+    let end_idx = stdout
+        .find(end_str)
+        .expect("Failed to find end_str in stdout");
 
     let deps_str = stdout[start_idx + start_str.len()..end_idx].trim();
-    let deps : Vec<&str> = deps_str.lines().map(|line| line.trim()).collect();
+    let deps: Vec<&str> = deps_str
+        .lines()
+        .map(|line| line.trim())
+        .filter(|line| !line.is_empty())
+        .filter(|line| *line != "Image has the following delay load dependencies:")
+        .collect();
 
-    println!("deps: {:?}", deps);
+    let target_dir: PathBuf = args
+        .target
+        .parent()
+        .expect(&format!(
+            "Failed to get parent dir for [{:?}]",
+            &args.target
+        ))
+        .into();
+
+    let mut table = prettytable::Table::new();
+    table.set_titles(prettytable::row![
+        "Dependency",
+        "Resolved Location (best guess)"
+    ]);
+    //table.add_row(prettytable::row!["Dep", "Loc"]);
+    for dep in &deps {
+        let loc_str = match which::which(dep)
+            .or_else(|_| which::which_in(dep, Some(&target_dir), &target_dir))
+        {
+            Ok(path) => path.to_string_lossy().into_owned(),
+            Err(_) => "⚠ Not Found ⚠".to_owned(),
+        };
+
+        table.add_row(prettytable::row![dep, loc_str.as_str()]);
+        //println!("{dep} ==> {loc_str}");
+    }
+    table.set_format(*prettytable::format::consts::FORMAT_NO_LINESEP_WITH_TITLE);
+    table.printstd();
 
     Ok(())
 }
